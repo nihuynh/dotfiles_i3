ea0cb6d83385a170cf7b080906abf3ffda8dd7cb
Nicolas Huynh <nicolas.huynh@stormshield.eu>
2021-03-16 12:04:55 +0100

    Config i3 update

--- .config/i3/screen_setup.sh
+++ .config/i3/screen_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PRIMARY_BG=~/Pictures/sm-navette-spatiale-iss-station-spatiale-01.jpg
-SECOND_BG=~/Pictures/hubble-1080x1920-space-galaxy-8k-17387.jpg
+PRIMARY_BG=$HOME/.config/i3/hubble.jpg
+SECOND_BG=$HOME/.config/i3/portrait.jpg
 
 xrandr \
-	--output eDP-1-1 --primary --mode 1920x1080 --pos 0x840 --rotate normal \
-	--output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate right
+	--output eDP-1 --primary --mode 1920x1080 --pos 0x840 --rotate normal \
+	--output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate left
 sleep 1
 feh --bg-scale $PRIMARY_BG --bg-scale $SECOND_BG
