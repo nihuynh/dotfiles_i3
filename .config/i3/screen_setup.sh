c953a03de15d028d28ca27d016b665e60ced0787
Nicolas Huynh <nicolas.huynh@stormshield.eu>
2021-09-24 09:49:15 +0200

    small tweaks

--- .config/i3/screen_setup.sh
+++ .config/i3/screen_setup.sh
@@ -13,17 +13,16 @@ VERTICAL_SCREEN=DP-1-8
 sleep 1
 if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 3 ]]; then
 	xrandr \
-	--output $PRIMARY_SCREEN --primary --pos 0x320 \
-	--output $VERTICAL_SCREEN --pos 1920x0 --rotate right \
-	--output $INTR_PORT --pos 0x1400
+	--output $PRIMARY_SCREEN --primary \
+	--output $VERTICAL_SCREEN --rotate right \
+	--output $INTR_PORT
 	sleep 1
 	feh --bg-scale $PRIMARY_BG --bg-scale $PRIMARY_BG --bg-scale $SECOND_BG
 else
 	xrandr \
 	--output $INTR_PORT --primary --pos 0x320 \
-	--output HDMI-1 --pos 1920x0 --rotate right
+	--output HDMI-1 --pos 1920x0 --rotate left
 	sleep 1
 	feh --bg-scale $PRIMARY_BG --bg-scale $SECOND_BG
 fi
-xrandr --output $INTR_PORT --primary
 xinput set-button-map 12 1 0 3
